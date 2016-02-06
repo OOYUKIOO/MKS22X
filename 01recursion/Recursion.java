33d0a1ab0f2bfe7096665a23c0a627dd819b918b
Yuxuan <ychen14@stuy.edu>
2016-02-06 18:52:58 -0500

    Success throwing and catching exception for method sqrt

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -1,5 +1,30 @@
 public class Recursion{
 
 
+    //REMEMBER TO REMOVE ALL STATIC!!!!!!!!!!!!!!!!!!!!
+
+
+    public static String name(){
+	return "Chen,Yuxuan";
+    }
+
+    
+    public static double sqrt(double n){
+	if(n<0){
+	    throw new IllegalArgumentException();
+	}
+	return 0;
+    }
+
+
+    public static void main(String[]args){
+	try{
+	    sqrt(-1);
+	}catch(IllegalArgumentException e){
+	    System.out.println("caught!");
+	}
+	System.out.println(name());
+    }
+
 
 }
