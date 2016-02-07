bb4a424fff7c12dcc4d92e195ff55e133d712d3e
Yuxuan <ychen14@stuy.edu>
2016-02-06 19:07:56 -0500

    Success returning square root of parameter n

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -10,10 +10,20 @@ public class Recursion{
 
     
     public static double sqrt(double n){
+	//throw exception when n is negative
 	if(n<0){
 	    throw new IllegalArgumentException();
 	}
-	return 0;
+	return sqrt_helper(n,n/2);
+    }
+
+    public static double sqrt_helper(double n, double guess){
+	//exit case: percentage error less than .001%
+	if((Math.abs(Math.pow(guess,2)-n)/n)*100 <0.001){
+	    return guess;
+	}
+	guess = (n/guess + guess)/2;
+	return sqrt_helper(n,guess);
     }
 
 
@@ -24,6 +34,7 @@ public class Recursion{
 	    System.out.println("caught!");
 	}
 	System.out.println(name());
+	System.out.println(sqrt(100));
     }
 
 
