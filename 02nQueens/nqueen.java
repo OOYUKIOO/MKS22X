003a975c750d55b4cb55b49ad5758a700fb1a66d
Yuxuan <ychen14@stuy.edu>
2016-02-10 01:01:42 -0500

    Create new class nqueen, create variable and constructor

---
+++ 02nQueens/nqueen.java
@@ -0,0 +1,16 @@
+public class nqueen{
+
+    //variable
+    int size;
+    int[][]board;
+
+    //constructor
+    public nqueen(int size){
+	this.size = size;
+	board = new int[size][size];
+    }
+
+
+
+
+}
