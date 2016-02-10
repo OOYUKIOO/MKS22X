449520eff9d462733eb127512b8224af924e0787
Yuxuan <ychen14@stuy.edu>
2016-02-10 01:41:55 -0500

    Add method for printing out chessboard visually

--- 02nQueens/nqueen.java
+++ 02nQueens/nqueen.java
@@ -2,7 +2,7 @@ public class nqueen{
 
     //variable
     int size;
-    int[][]board;
+    int[][] board;
 
     //constructor
     public nqueen(int size){
@@ -10,7 +10,35 @@ public class nqueen{
 	board = new int[size][size];
     }
 
+    //returns value for size
+    public int getSize(){
+	return size;
+    }
 
+
+    //add queen
+    public void addQueen(int row, int col){
+
+    }
+
+    //for testing purpose
+    public String chessboard(){
+	String visual = "";
+	for(int row = 0; row < size; row++){
+	    for (int col = 0; col < size; col++){
+		visual += board[row][col]+" ";
+	    }
+	    visual+= "\n";
+	}
+	return visual;
+    }
+
+    //test
+    public static void main(String[]args){
+	nqueen x = new nqueen(4);
+	String a = x.chessboard();
+	System.out.println(a);
+    }
 
 
 }
