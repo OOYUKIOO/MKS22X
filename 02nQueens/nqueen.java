35ce0d24428cee5fd8f518982ec7724f1224d5e7
Yuxuan <ychen14@stuy.edu>
2016-02-10 02:01:11 -0500

    Successfully add removeQueen method, testing passes, fail to push to gitHub server

--- 02nQueens/nqueen.java
+++ 02nQueens/nqueen.java
@@ -13,7 +13,7 @@ public class nqueen{
 
     //add queen
     public void addQueen(int row, int col){
-	for(int pcol = 0; pcol < size; pcol++){
+ 	for(int pcol = 0; pcol < size; pcol++){
 	    board[row][pcol] = board[row][pcol]-1;
 	}
 	for(int prow = 0; prow < size; prow++){
@@ -22,12 +22,23 @@ public class nqueen{
 	board[row][col] = 1;
     }
 
+    //remove queen
+    public void removeQueen(int row, int col){
+	for(int pcol = 0; pcol < size; pcol++){
+	    board[row][pcol] = board[row][pcol]+1;
+	}
+	for (int prow = 0; prow < size; prow++){
+	    board[prow][col] = board[prow][col]+1;
+	}
+	board[row][col] = 0;
+    }
+
     //for testing purpose
     public String chessboard(){
 	String visual = "";
 	for(int row = 0; row < size; row++){
 	    for (int col = 0; col < size; col++){
-		visual += board[row][col]+" ";
+		visual += board[row][col]+"  ";
 	    }
 	    visual+= "\n";
 	}
@@ -39,9 +50,18 @@ public class nqueen{
 	nqueen x = new nqueen(4);
 	String a = x.chessboard();
 	System.out.println(a);
+
 	x.addQueen(0,2);
 	String b = x.chessboard();
 	System.out.println(b);
+
+	x.addQueen(2,3);
+	String c = x.chessboard();
+	System.out.println(c);
+
+	x.removeQueen(2,3);
+	String d = x.chessboard();
+	System.out.println(d);
     }
 
 
