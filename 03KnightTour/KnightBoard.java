9c2f37fbe9aa0e1c24dc32120e5c650110d695ab
Yuxuan <ychen14@stuy.edu>
2016-02-14 21:52:02 -0500

    Finish constructor

--- 03KnightTour/KnightBoard.java
+++ 03KnightTour/KnightBoard.java
@@ -1,11 +1,23 @@
 public class KnightBoard{
+    //variables
+    public int[][]board;
+
 
     //constructor
-
+    public KnightBoard(int size){
+	board = new int[size][size];
+	//initial all to -1, unvisitied
+	for(int row = 0; row < board.length; row++){
+	    for (int col = 0; col < board[row].length; col++){
+		board[row][col] = -1;
+	    }
+	}
+    }
 
 
     public boolean solve(){
 
+	return false;
     }
 
     public void printSolution(){
