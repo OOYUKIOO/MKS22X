41ee4c15c16a32ab1e2a74ca6f68ee4731ce21b1
Yuxuan <ychen14@stuy.edu>
2016-02-14 21:59:49 -0500

    Test printSolution function

--- 03KnightTour/KnightBoard.java
+++ 03KnightTour/KnightBoard.java
@@ -16,12 +16,31 @@ public class KnightBoard{
 
 
     public boolean solve(){
+	return solveHelper(0,0,0);
+    }
+
+    //helper function
+    public boolean solveHelper(int row, int col, int move){
 
 	return false;
     }
 
     public void printSolution(){
-
+	String visual = "";
+	for(int row = 0; row < board.length; row++){
+	    for(int col = 0; col < board[row].length; col++){
+		visual += board[row][col]+" ";
+	    }
+	    visual += "\n";
+	}
+	System.out.println(visual);
     }
+
+    //testing
+    public static void main(String[]args){
+	KnightBoard a = new KnightBoard(3);
+	a.printSolution();
+    }
+
 
 }
