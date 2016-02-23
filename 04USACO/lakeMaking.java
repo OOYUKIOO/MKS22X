9afbd145057c7c2f1e864183ab281f76b272b1cb
Yuxuan <ychen14@stuy.edu>
2016-02-22 20:50:32 -0500

    Use Scanner to read in input file

--- 04USACO/lakeMaking.java
+++ 04USACO/lakeMaking.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+import java.io.*;
 public class lakeMaking{
+    int row;
+    int col;
+    int depth;
+    int commands;
+    int[] heights;
+    Scanner input;
+
+    //read in file input
+    public lakeMaking(){
+	try{
+	    input = new Scanner(new File("input.txt"));
+	}catch(FileNotFoundException e){
+	    System.out.println("oops");
+	}
+    }
 
 
-
-
+    //testing
+    public static void main(String[]args){
+	lakeMaking x = new lakeMaking();
+    }
 
 
 
