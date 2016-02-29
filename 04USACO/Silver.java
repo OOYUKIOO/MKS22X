45a09e81b114c63ce50cd236d495d2f3301c1407
Yuxuan <ychen14@stuy.edu>
2016-02-29 12:18:16 -0500

    Add testing input file, start constructor

--- 04USACO/Silver.java
+++ 04USACO/Silver.java
@@ -6,7 +6,15 @@ public class Silver{
     int[][]pasture;
     int startx,starty,endx,endy;
 
-
-
+    //constructor
+    public Silver(){
+	Scanner input;
+	try{
+	    input = new Scanner(new File("cowTravelling.in"));
+	}catch(FileNotFoundException e){
+	    System.out.println("Input file not found!");
+	}
+	row = input.nextInt();
+	col = input.nextInt();
 
 }
