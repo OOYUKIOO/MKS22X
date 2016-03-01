795f63c22182df6358b3c8b8c989e857b2b9bfc9
Yuxuan <ychen14@stuy.edu>
2016-02-29 21:59:48 -0500

    Tring to add a padding around array pasture, not yet finished

--- 04USACO/Silver.java
+++ 04USACO/Silver.java
@@ -17,17 +17,66 @@ public class Silver{
 	row = input.nextInt();
 	col = input.nextInt();
 	steps = input.nextInt();
-	//	System.out.println("" + row + ", " + col + ", " + steps);
-	pasture = new int[row][col];
+	pasture = new int[row+2][col+2];
+	//make surrounding pad and tree -1
 	for (int y = 0; y < pasture.length; y++){
+	    if(y == 0 || y == pasture.length-1){
+		for(int x = 0; x < pasture[y].length; x++){
+		    pasture[y][x] = -1;
+		}
+	    }
 	    String rowElement = input.next();
 	    for(int x = 0; x < rowElement.length(); x++){
 		if(rowElement.charAt(x) == '*'){
+		    pasture[y+1][x+1] = -1;
+		}
+		if(x == 0 || x == pasture[0].length-1){
 		    pasture[y][x] = -1;
 		}
 	    }
 	}
+    }
 
+    //to solve
+    public int solve(){
+	pasture[startx][starty] = 1;
+	while(steps > 0){
+	    int [] temp = new int [col*row - 1];
+	    for(int prow = 0; prow < pasture.length; prow++){
+		for (int pcol = 0; pcol < pasture[prow].length; pcol++){
+		    /*
+		    //top side
+		    if(prow == 0){
+			if(pcol == 0){
+			    temp[pcol] = pasture[prow+1][pcol] + pasture[prow][pcol+1];
+			}else if(pcol == col-1){
+			    temp[pcol] = pasture[prow+1][pcol] + pasture[prow][pcol-1];
+			}else{
+			    temp[pcol] = pasture[prow][pcol-1] + pasture[prow][pcol+1] + pasture[prow+1][pcol];
+			}
+			//bottom side
+		    }else if(prow == row-1){
+			if(pcol == 0){
+			    temp[prow*col+pcol] = pasture[prow-1][pcol] + pasture[prow][pcol+1];
+			}else if(pcol == col-1){
+			    temp[prow*col+pcol] = pasture[prow-1][pcol] + pasture[prow][pcol-1];
+			}else{
+			    temp[prow*col+pcol] = pasture[prow][pcol-1] + pasture[prow][pcol+1] + pasture[prow-1][pcol];
+			}
+		    //left side
+		    }else if(pcol == 0){
+			temp[prow*col+pcol] = pasture[prow-1][pcol] + pasture[prow+1][pcol] + pasture[prow][pcol+1];
+			//right side
+		    }else if(pcol == col-1){
+			temp[prow*col+pcol] = pasture[prow-1][pcol] + pasture[prow+1][pcol] + pasture[prow][pcol-1];
+		    }else{
+			temp[prow*col+pcol] = pasture[prow-1][pcol] + pasture[prow+1][pcol] + pasture[prow][pcol-1] + pasture[prow][pcol+1];
+		    }
+		    */
+		}
+	    }
+	}
+	return pasture[endx][endy];
     }
 
     //for debugging purpose
