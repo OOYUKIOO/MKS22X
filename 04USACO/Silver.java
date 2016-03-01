6f45daf0b2ed93638ff66db55a8cf7fbb3b6be83
Yuxuan <ychen14@stuy.edu>
2016-02-29 20:51:32 -0500

    Working on constructor, able to mark trees on map as -1

--- 04USACO/Silver.java
+++ 04USACO/Silver.java
@@ -8,7 +8,7 @@ public class Silver{
 
     //constructor
     public Silver(){
-	Scanner input;
+	Scanner input = new Scanner("");
 	try{
 	    input = new Scanner(new File("cowTravelling.in"));
 	}catch(FileNotFoundException e){
@@ -16,5 +16,37 @@ public class Silver{
 	}
 	row = input.nextInt();
 	col = input.nextInt();
+	steps = input.nextInt();
+	//	System.out.println("" + row + ", " + col + ", " + steps);
+	pasture = new int[row][col];
+	for (int y = 0; y < pasture.length; y++){
+	    String rowElement = input.next();
+	    for(int x = 0; x < rowElement.length(); x++){
+		if(rowElement.charAt(x) == '*'){
+		    pasture[y][x] = -1;
+		}
+	    }
+	}
 
+    }
+
+    //for debugging purpose
+    public String toString(){
+	String ans = "";
+	for(int y = 0; y < pasture.length; y++){
+	    for(int x = 0; x < pasture[y].length; x++){
+		ans += pasture[y][x] + " ";
+	    }
+	    ans += "\n";
+	}
+	return ans;
+    }
+
+    //testing
+    public static void main(String[]args){
+	Silver x = new Silver();
+	System.out.println(x);
+    } 
+
+   
 }
