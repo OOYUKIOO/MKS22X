7de043320fe51a2405b396a7e211de3bf50df853
Yuxuan <ychen14@stuy.edu>
2016-03-03 12:04:55 -0500

    Success merging two parts into new array

--- 06merge/merge.java
+++ 06merge/merge.java
@@ -1,11 +1,45 @@
 public class merge{
 
+public static void printArray(int[]data){
+	String message = "[";
+	if (data.length==0){
+	    message+=" ";
+	}
+	for(int i=0;i<data.length;i++){
+	    message += data[i]+",";
+	}
+	System.out.println(message.substring(0,message.length()-1)+"]");
+    }
+
     /*
       The function can take an array, and the start/end indices of each part to be merged. it could then merge into a new array, and copy the results back.
     */
-    public void merge(int[]data, int startA, int endA, int startB, int endB){
-	int[] temp = new int[(startA-endA+1) + (startB-endB+1)];
+    public static void merge(int[]data, int startA, int endA, int startB, int endB){
+	int[] temp = new int[(endA-startA+1) + (endB-startB+1)];
+	for(int i = 0; i < temp.length; i++){
+	    if(startA > endA){
+		temp[i] = data[startB];
+		startB++;
+	    }else if(startB > endB){
+		temp[i] = data[startA];
+		startA++;
+	    }else{
+		temp[i] = Math.min(data[startA], data[startB]);
+		if(temp[i] == data[startA]){
+		    startA++;
+		}else{
+		    startB++;
+		}
+	    }
+	}
+	printArray(temp);
     }
 
+
+    //testing
+    public static void main(String[]args){
+	int[] x = {1,5,8,2,4,10};
+	merge(x,0,2,3,5);
+    }
 
 }
