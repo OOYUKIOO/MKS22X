c2e5ca9d698daadf706f3ba7b5f8556730acec96
Yuxuan <ychen14@stuy.edu>
2016-03-03 12:10:17 -0500

    Success adding array back to original data array

--- 06merge/merge.java
+++ 06merge/merge.java
@@ -15,6 +15,7 @@ public static void printArray(int[]data){
       The function can take an array, and the start/end indices of each part to be merged. it could then merge into a new array, and copy the results back.
     */
     public static void merge(int[]data, int startA, int endA, int startB, int endB){
+	int start = startA;
 	int[] temp = new int[(endA-startA+1) + (endB-startB+1)];
 	for(int i = 0; i < temp.length; i++){
 	    if(startA > endA){
@@ -33,13 +34,18 @@ public static void printArray(int[]data){
 	    }
 	}
 	printArray(temp);
+	for(int i = 0; i < temp.length; i++){
+	    data[start] = temp[i];
+	    start++;
+	}
+	printArray(data);
     }
 
 
     //testing
     public static void main(String[]args){
-	int[] x = {1,5,8,2,4,10};
-	merge(x,0,2,3,5);
+	int[] x = {1,5,2,8,4,10};
+	merge(x,2,3,4,5);
     }
 
 }
