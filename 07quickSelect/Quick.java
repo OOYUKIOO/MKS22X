9b0570179c6248a062edb57c71f4e2b033594fdd
Yuxuan <ychen14@stuy.edu>
2016-03-08 12:13:04 -0500

    moving stuff overrrrrrr

--- 07quickSelect/Quick.java
+++ 07quickSelect/Quick.java
@@ -2,16 +2,27 @@ import java.util.Random;
 
 public class Quick{
 
-    private static int partition(int[]data, int left, int right){
+    Random rand = new Random();
 
+    private static int partition(int[]data, int left, int right){
+	return 0;
     }
 
     public static int quickselect(int[]data, int k){
-
+	return 0;
     }
 
     private static int quickselect(int[]data, int k, int left, int right){
-
+	int leftI = left;
+	int rightI = right;
+	while(left != right){
+	    int pos = rand.nextInt(right - left);
+	    int selected = data[pos];
+	    data[pos] = data[right];
+	    data[right] = selected;
+	    right --;
+	}
+	return 0;
     }
 
 
