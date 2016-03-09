9c6680c838a3f72482a5941a65e0233e3f3e6af4
Yuxuan <ychen14@stuy.edu>
2016-03-08 20:27:15 -0500

    Compiles, now writing main to test the code

--- 07quickSelect/Quick.java
+++ 07quickSelect/Quick.java
@@ -3,15 +3,26 @@ import java.util.Random;
 public class Quick{
 
 
+    private static int quickselect(int[]data, int k){
+	return quickselect(data,k,0,data.length-1);
+	
+    }
+
+    
+    public static int quickselect(int[]data, int k, int left, int right){
+	int pos = partition(data,left,right);
+	if(pos == k){
+	    return data[k];
+	}
+	if(pos < k){
+	    return  quickselect(data,k,pos+1,right);
+	}else{
+	    return quickselect(data,k,left,pos-1);
+	}
+    }
+    
+
     private static int partition(int[]data, int left, int right){
-	return 0;
-    }
-
-    public static int quickselect(int[]data, int k){
-	return 0;
-    }
-
-    private static int quickselect(int[]data, int k, int left, int right){
 	Random rand = new Random();
 
 	int leftI = left;
@@ -27,10 +38,28 @@ public class Quick{
 		left++;
 	    }else{
 		data[left] = data[right];
+		data[right] = temp;
+		right--;
 	    }
 	}
-	return 0;
+	if(data[left] > selected){
+	    left++;
+	}
+	data[rightI] = data[left];
+	data[left] = selected;
+	/*
+	if(left == k){
+	    return data[left];
+	}else if(left > k){
+	    return quickselect(data, k, leftI, left-1);
+	}else{
+	    return quickselect(data, k, left+1, rightI);
+	}
+	*/
+	return left;
     }
 
+
+    //testing
 
 }
