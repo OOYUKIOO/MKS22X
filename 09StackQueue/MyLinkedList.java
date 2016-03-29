b8f0b0e28e816cc68ea16e84d349a16efe331a75
Yuxuan <ychen14@stuy.edu>
2016-03-28 21:58:10 -0400

    Done and debug MyStack class

--- 09StackQueue/MyLinkedList.java
+++ 09StackQueue/MyLinkedList.java
@@ -277,7 +277,11 @@ public class MyLinkedList<T> implements Iterable<T>{
 	}
 
 	public T peek(){
-	    return queue.get(0);
+	    if(isEmpty()){
+		throw new NoSuchElementException();
+	    }else{
+		return queue.get(0);
+	    }
 	}
 
 	public int size(){
@@ -299,9 +303,69 @@ public class MyLinkedList<T> implements Iterable<T>{
 
     }
 
+
+    //MyStack
+    public static class MyStack<T>{
+	private int size;
+	private T item;
+	private MyLinkedList<T> stack;
+	
+	public MyStack(){
+	    this(0);
+	}
+	
+	public MyStack(int size){
+	    this.size = size;
+	    stack = new MyLinkedList<T>(size);
+	}
+
+	public void push(T item){
+	    stack.add(0,item);
+	    size++;
+	}
+	
+	
+	public T pop(){
+	    if(isEmpty()){
+		throw new NoSuchElementException();
+	    }else{
+		T ans = stack.get(0);
+		stack.remove(0);
+		size--;
+		return ans;
+	    }
+	}
+	
+	public T peek(){
+	    if(isEmpty()){
+		throw new NoSuchElementException();
+	    }else{
+		return stack.get(0);
+	    }
+	}
+	
+	public int size(){
+	    return size;
+	}
+	
+	public boolean isEmpty(){
+	    if(size == 0){
+		return true;
+	    }else{
+		return false;
+	    }
+	}
+	
+	public String toString(){
+	    String ans = stack.toString();
+	    return ans;
+	}
+	
+	
+    }
+    
     //testing
     public static void main(String[]args){
-
 	boolean debugQueue = false;
 	boolean debugStack = true;
 
@@ -326,6 +390,28 @@ public class MyLinkedList<T> implements Iterable<T>{
 	    System.out.println(x.size());
 	    System.out.println(x.isEmpty());
  	}
+
+	if(debugStack){
+	    MyStack<Integer> x = new MyStack<Integer>();
+	    System.out.println(x);
+	    try{
+		x.peek();
+	    }catch(NoSuchElementException e){
+		System.out.println("Gotcha!");
+	    }
+	    System.out.println(x.isEmpty());
+	    for(int i = 0; i < 20; i ++){
+		x.push(new Integer(i));
+	    }
+	    System.out.println(x);
+	    System.out.println(x.pop());
+	    System.out.println(x.pop());
+	    System.out.println(x.pop());
+	    System.out.println(x.peek());
+	    System.out.println(x);
+	    System.out.println(x.size());
+	    System.out.println(x.isEmpty());
+	}
     }
 
 }
