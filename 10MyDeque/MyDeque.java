958f6758ac18bd89e0a34e318003389ce96fc315
Yuxuan <ychen14@stuy.edu>
2016-04-01 12:17:13 -0400

    warning

--- 10MyDeque/MyDeque.java
+++ 10MyDeque/MyDeque.java
@@ -1,10 +1,21 @@
+import java.util.Arrays;
 public class MyDeque<T>{
 
     private int start;
     private int end;
+    private int size;
     private T[] circular;
 
+    public MyDeque(Class<T> x){
+	@SuppressWarnings("unchecked")
+	    //	circular = (T[])new Object[10];
+	    final T[] circular = (T[]) Array.newInstance(x,10);
+	this.circular = circular;
+    }
 
+    private void resize(){
+
+    }
 
 
 }
