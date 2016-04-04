363db725a4acdbd0f2c36ceb9a41ce009c553ae0
Yuxuan <ychen14@stuy.edu>
2016-04-03 20:43:54 -0400

    basic constructor working

--- 10MyDeque/MyDeque.java
+++ 10MyDeque/MyDeque.java
@@ -5,17 +5,47 @@ public class MyDeque<T>{
     private int end;
     private int size;
     private T[] circular;
+    
+    @SuppressWarnings("unchecked")
+    public MyDeque(int size){
+	circular = (T[])new Object[size];
+	this.size = size;
+    }
 
-    public MyDeque(Class<T> x){
-	@SuppressWarnings("unchecked")
-	    //	circular = (T[])new Object[10];
-	    final T[] circular = (T[]) Array.newInstance(x,10);
-	this.circular = circular;
+    public MyDeque(){
+	this(10);
     }
 
     private void resize(){
-
+	MyDeque newCircular = new MyDeque(size*2);
+	for (int i = 0; i<circular.length; i++){
+	    //	    newCircular[i] = circular[i];
+	}
+	//	circular = newCircular;
     }
 
+    public void addFirst(T value){
+	circular[start] = value;
+	start++;
+    }
+
+
+
+
+    public String toString(){
+	String ans = "[";
+	for(int i = 0; i < circular.length; i++){
+	    ans+= circular[i] + ",";
+	}
+	return(ans.substring(0,ans.length()-1)+"]");
+    }
+
+
+
+    //testing
+    public static void main(String[]args){
+	MyDeque x = new MyDeque();
+	System.out.println(x);
+    }
 
 }
