3a768de1b38f7b8051b783044f543ba99fb25bb3
Yuxuan <ychen14@stuy.edu>
2016-04-04 22:29:16 -0400

    Add function needs change

--- 10MyDeque/MyDeque.java
+++ 10MyDeque/MyDeque.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 public class MyDeque<T>{
 
     private int start;
@@ -7,37 +7,77 @@ public class MyDeque<T>{
     private T[] circular;
     
     @SuppressWarnings("unchecked")
-    public MyDeque(int size){
-	circular = (T[])new Object[size];
-	this.size = size;
+    public MyDeque(int length){
+	circular = (T[])new Object[length];
+	size = 0;
     }
 
     public MyDeque(){
 	this(10);
     }
-
+    @SuppressWarnings("unchecked")
     private void resize(){
-	MyDeque newCircular = new MyDeque(size*2);
-	for (int i = 0; i<circular.length; i++){
-	    //	    newCircular[i] = circular[i];
+	T[] newCircular = (T[])new Object[size*2];
+	end = 0;
+	for (int i = start; i<circular.length; i++){
+	    newCircular[i-start] = circular[i];
+	    end++;
 	}
-	//	circular = newCircular;
+	/*
+	for (int i = 0; i<start; i++){
+	    newCircular[start+1+i]=circular[i];
+	    end++;
+	}
+	*/
+	start = 0;
+	circular = newCircular;
     }
 
     public void addFirst(T value){
-	circular[start] = value;
 	start--;
 	if(start < 0){
 	    start = circular.length-1;
 	}
+	if(start == end){
+	    resize();
+	}
+	circular[start] = value;
+	size++;
     }
 
     public void addLast(T value){
-	circular[end] = value;
 	end++;
+	if(end > circular.length-1){
+	    end = 0;
+	}
+	if(end == start){
+	    resize();
+	}
+	circular[end] = value;
+	size++;
     }
 
+    public T removeFirst(){
+	if(size==0){
+	    throw new NoSuchElementException();
+	}
+	T ans = circular[start];
+	circular[start] = null;
+	start++;
+	size--;
+	return ans;
+    }
 
+    public T removeLast(){
+	if(size == 0){
+	    throw new NoSuchElementException();
+	}
+	T ans = circular[end];
+	circular[end] = null;
+	end--;
+	size--;
+	return ans;
+    }
 
     public int getSize(){
 	return size;
@@ -60,12 +100,20 @@ public class MyDeque<T>{
     //testing
     public static void main(String[]args){
 	MyDeque<Integer> x = new MyDeque<Integer>();
-	for(int i = 0; i<4; i++){
+	for(int i = 0; i<5; i++){
 	    x.addFirst(new Integer(i));
 	}
 	x.debugString();
 	for(int i = 0; i<5; i++){
 	    x.addLast(new Integer(i));
+	}
+	x.debugString();
+	for(int i = 0; i<3; i++){
+	    x.removeFirst();
+	}
+	x.debugString();
+	for(int i = 0; i<4; i++){
+	    x.removeLast();
 	}
 	x.debugString();
     }
