f48a9f0379e4f897ed1741c63dd9717f25683f22
Yuxuan <ychen14@stuy.edu>
2016-04-11 23:49:45 -0400

    write bettermaze class

---
+++ 11frontier/BetterMaze.java
@@ -0,0 +1,12 @@
+public class BetterMaze{
+
+    private char[][]maze;
+    private int[] solution;
+    private int startRow, startCol;
+    private Frontier<Node> placesToGO;
+    private boolean animate = false;
+
+
+
+
+}
