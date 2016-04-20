d853ef1e33dda4b88010b4ef43d080bd1528e05d
Yuxuan <ychen14@stuy.edu>
2016-04-20 12:13:03 -0400

    Starting BSTree

---
+++ 12BSTree/BSTree.java
@@ -0,0 +1,31 @@
+public class BSTree<T extends Comparable<T>>{
+
+    public int compareTo(BSTree n){
+	return 0;
+    }
+
+    Node root;
+
+    private class Node{
+
+	Node left, right;
+	T data;
+
+	public T get(){
+	    return T;
+	}
+
+	public void set(T value){
+	    data = value;
+	}
+
+	public void add(T value){
+
+	}
+
+
+    }
+
+
+
+}
