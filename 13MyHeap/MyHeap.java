f7fd1a00662beebbf475d4a93c6195a1c8002128
Yuxuan <ychen14@stuy.edu>
2016-05-05 12:02:13 -0400

    pushUp and pushDown

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -3,8 +3,17 @@ public class MyHeap<T extends Comparable<T>>{
     T[] heap;
 
 
+    private void pushUp(int index){
+	T child = heap[index];
+	heap[index] = heap[index/2];
+	heap[index/2] = child;
+    }
 
-
+    private void pushDown(int index, int shift){
+	T parent = heap[index];
+	heap[index] = heap[index*2+shift];
+	heap[index*2+shift] = parent;
+    }
 
 
 
