da277b3f3252b94e03c1c7a14b1bd317d0ba06bf
Yuxuan <ychen14@stuy.edu>
2016-05-10 11:58:02 -0400

    starting RunningMedian class

---
+++ 14RunningMedian/RunningMedian.java
@@ -0,0 +1,20 @@
+public class RunningMedian{
+
+    private MyHeap<Integer> maxHeap;
+    private MyHeap<Integer> minHeap;
+
+    public RunningMedian(){
+
+    }
+
+    public double getMedian(){
+	double med = 0.;
+	return med;
+    }
+
+    public void add(Integer x){
+
+    }
+
+
+}
