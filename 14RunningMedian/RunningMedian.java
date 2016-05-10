0f25cce6b33e6ab8f0418e4c32d60cc226c3508b
Yuxuan <ychen14@stuy.edu>
2016-05-10 12:07:45 -0400

    writing getMedian()

--- 14RunningMedian/RunningMedian.java
+++ 14RunningMedian/RunningMedian.java
@@ -1,19 +1,34 @@
+import java.util.*;
 public class RunningMedian{
 
     private MyHeap<Integer> maxHeap;
     private MyHeap<Integer> minHeap;
 
     public RunningMedian(){
-
+	maxHeap = new MyHeap<Integer>();
+	minHeap = new MyHeap<Integer>();
     }
 
     public double getMedian(){
+	if(maxHeap.getSize() + minHeap.getSize() == 0){
+	    throw new NoSuchElementException();
+	}
 	double med = 0.;
+	if(maxHeap.getSize() == minHeap.getSize()){
+	    med = (maxHeap.peek() + minHeap.peek())/2.;
+	}else if (maxHeap.getSize() > minHeap.getSize()){
+	    med = maxHeap.peek();
+	}else{
+	    med = minHeap.peek();
+	}
 	return med;
     }
 
     public void add(Integer x){
 
+	if(maxHeap.getSize() == minHeap.getSize()){
+
+	}
     }
 
 
