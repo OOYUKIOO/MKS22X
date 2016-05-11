4a4c10c9376e306ad1e56d10cca637b67fed1478
Yuxuan <ychen14@stuy.edu>
2016-05-10 21:09:47 -0400

    Finishgit add RunningMedian.java!

--- 14RunningMedian/RunningMedian.java
+++ 14RunningMedian/RunningMedian.java
@@ -4,6 +4,14 @@ public class RunningMedian{
     private MyHeap<Integer> maxHeap;
     private MyHeap<Integer> minHeap;
 
+    private boolean DEBUG = false;;
+    public void debug(String s){
+	if(DEBUG){
+	    System.out.println(s);
+	}
+    }
+
+
     public RunningMedian(){
 	maxHeap = new MyHeap<Integer>();
 	minHeap = new MyHeap<Integer>();
@@ -11,6 +19,8 @@ public class RunningMedian{
 
     public double getMedian(){
 	if(maxHeap.getSize() + minHeap.getSize() == 0){
+	    System.out.println(maxHeap);
+	    System.out.println(minHeap);
 	    throw new NoSuchElementException();
 	}
 	double med = 0.;
@@ -25,26 +35,37 @@ public class RunningMedian{
     }
 
     public void add(Integer x){
-	double med = getMedian();
+	double med;
+	try{
+	    med = getMedian();
+	}catch(NoSuchElementException e){
+	    med = 0;
+	}
 	if(maxHeap.getSize() == minHeap.getSize()){
 	    if(x <= med){
+		debug("add to left");
 		maxHeap.add(x);
 	    }else{
+		debug("add to right");
 		minHeap.add(x);
 	    }
 	}else if(maxHeap.getSize() > minHeap.getSize()){
 	    if(x >= med){
+		debug("add to right");
 		minHeap.add(x);
 	    }else{
 		Integer moveOver = maxHeap.delete();
+		debug("add to left");
 		maxHeap.add(x);
 		minHeap.add(moveOver);
 	    }
 	}else{
 	    if(x <= med){
+		debug("add to left");
 		maxHeap.add(x);
 	    }else{
 		Integer moveOver = minHeap.delete();
+		debug("add to right");
 		minHeap.add(x);
 		maxHeap.add(moveOver);
 	    }
@@ -52,9 +73,30 @@ public class RunningMedian{
     }
 
     public String toString(){
-	String ans="";
-	return ans;
-
+	String left = maxHeap.toString().substring(0,maxHeap.toString().length()-2);
+	String right = minHeap.toString().substring(1);
+	try{
+	    return left + " |" + right + " Median = " + getMedian();
+	}catch(NoSuchElementException e){
+	    return left + " |" + right + " Median = N/A";
+	}
+	
     }
 
+
+    public static void main(String[]args){
+
+	RunningMedian a = new RunningMedian();
+	System.out.println(a);
+	a.add(new Integer(2));
+	a.add(new Integer(1));
+	a.add(new Integer(9));
+	a.add(new Integer(4));
+	a.add(new Integer(7));
+	a.add(new Integer(0));
+	a.add(new Integer(-4));
+	System.out.println(a);
+
+
+    }
 }
